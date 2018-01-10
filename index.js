baff67e6788b62ed9bd656a583d39d4b3edce231
Lou <luana.martelli@heig-vd.ch>
2018-01-10 08:41:31 +0100

    Remove extra in server file

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ const PORT = process.env.PORT || 5000
 var app = express()
 
 app
-  .get('/', function(req, res)) {
+  .get('/', function(req, res) {
   	res.send('Good Morning Dear Sir !')	
-  }
-
-  app
+  })
   //.use(express.static(path.join(__dirname, 'public')))
   //.set('views', path.join(__dirname, 'views'))
   //.set('view engine', 'ejs')
