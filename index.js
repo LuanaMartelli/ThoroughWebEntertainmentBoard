a9bf1d223b4a08de1c119abaeb7169c08abccece
Nadir Benallal <nadir.benallal@heig-vd.ch>
2018-01-16 16:56:52 +0100

    Refactor du code de index.js en promesses

--- index.js
+++ index.js
@@ -5,40 +5,58 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app
-  .get('/', (req, res) => {
-    res.send('Good Morning Dear Sir !');
-  })
-  .post('/gimme', (req, res) => {
-    request.get({
-      headers: {
-        Accept: 'application/vnd.github.v3+json',
-        'User-Agent': 'ThoroughWebEntertainmentBoard',
-      },
-      url: `https://api.github.com/users/${req.query.username}`,
-    }, (error, response, body) => {
-      if (error) {
-        res.send(`error: ${error}`);
-      } else if (response.statusCode !== 200) {
-        res.send(`status: ${request.statusCode}`);
-      } else {
-        let result = {
-          username: body.login,
-          avatar: body.avatar_url,
-        };
+function getUser(callback) {
+  app
+    .get('/', (req, res) => {
+      res.send('Good Morning Dear Sir !');
+    })
+    .post('/gimme', (req, res) => {
+      request.get({
+        headers: {
+          Accept: 'application/vnd.github.v3+json',
+          'User-Agent': 'ThoroughWebEntertainmentBoard',
+        },
+        url: `https://api.github.com/users/${req.query.username}`,
+      }, (error, response, body) => {
+        if (error) {
+          res.send(`error: ${error}`);
+        } else if (response.statusCode !== 200) {
+          res.send(`status: ${request.statusCode}`);
+        } else {
+          res.send(body);
+        }
+      });
+    })
+    // .use(express.static(path.join(__dirname, 'public')))
+    // .set('views', path.join(__dirname, 'views'))
+    // .set('view engine', 'ejs')
+    // .get('/', (req, res) => res.render('pages/index'))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+}
 
-        /*
-        TODO faire toutes les requête nécaissaires pour obtenir toutes les infos désirées
-        et les renvoyer dans un gros JSON qui contient tout ce dont on a besoin.
-
-        Faire avec des promesses serait bien
-        */
-        res.send(body);
-      }
+function fetchUser(context) {
+  console.log('getting user informations');
+  return new Promise((resolve, reject) => {
+    getUser((error, data) => {
+      if (error) reject(error);
+      else resolve(data);
     });
+  });
+}
+
+function fetchData() {
+  console.log('getting data');
+  const context = {};
+
+  return fetchUser(context);
+}
+
+fetchData()
+  .then((result) => {
+    console.log('sucess!');
+    console.log(result);
   })
-  // .use(express.static(path.join(__dirname, 'public')))
-  // .set('views', path.join(__dirname, 'views'))
-  // .set('view engine', 'ejs')
-  // .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+  .catch((error) => {
+    console.log('failure!');
+    console.log(error);
+  });
