cebce147abbc81e107e62caa19ec4f69c5ab140d
Nadir Benallal <nadir.benallal@heig-vd.ch>
2018-01-17 09:11:40 +0100

    Simple promises implemented for fetching user and repos

--- index.js
+++ index.js
@@ -1,62 +1,43 @@
 const express = require('express');
-const request = require('request');
+const rp = require('request-promise');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-function getUser(callback) {
-  app
-    .get('/', (req, res) => {
-      res.send('Good Morning Dear Sir !');
-    })
-    .post('/gimme', (req, res) => {
-      request.get({
-        headers: {
-          Accept: 'application/vnd.github.v3+json',
-          'User-Agent': 'ThoroughWebEntertainmentBoard',
-        },
-        url: `https://api.github.com/users/${req.query.username}`,
-      }, (error, response, body) => {
-        if (error) {
-          res.send(`error: ${error}`);
-        } else if (response.statusCode !== 200) {
-          res.send(`status: ${request.statusCode}`);
-        } else {
-          res.send(body);
-        }
-      });
-    })
-    // .use(express.static(path.join(__dirname, 'public')))
-    // .set('views', path.join(__dirname, 'views'))
-    // .set('view engine', 'ejs')
-    // .get('/', (req, res) => res.render('pages/index'))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
-}
-
-function fetchUser(context) {
-  console.log('getting user informations');
-  return new Promise((resolve, reject) => {
-    getUser((error, data) => {
-      if (error) reject(error);
-      else resolve(data);
-    });
+function fetchGithub(url) {
+  return rp({
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      'User-Agent': 'ThoroughWebEntertainmentBoard',
+    },
+    url,
+    json: true,
   });
 }
 
-function fetchData() {
-  console.log('getting data');
-  const context = {};
-
-  return fetchUser(context);
-}
-
-fetchData()
-  .then((result) => {
-    console.log('sucess!');
-    console.log(result);
+app
+  .get('/', (req, res) => {
+    res.send('Good Morning Dear Sir !');
   })
-  .catch((error) => {
-    console.log('failure!');
-    console.log(error);
-  });
+  .post('/gimme', (req, res) => {
+    const context = {};
+    fetchGithub(`https://api.github.com/users/${req.query.username}`)
+      .then((user) => {
+        context.username = user.login;
+        return fetchGithub(user.repos_url);
+      })
+      .then((repos) => {
+        context.repo = repos[0].full_name;
+        res.send(context);
+      }).catch((error) => {
+        console.log('error:');
+        console.log(error);
+        res.send(error);
+      });
+  })
+  // .use(express.static(path.join(__dirname, 'public')))
+  // .set('views', path.join(__dirname, 'views'))
+  // .set('view engine', 'ejs')
+  // .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
