1536570062e86b6dbd156837881c691a596edcd2
Nadir Benallal <nadir.benallal@heig-vd.ch>
2018-01-22 21:22:12 +0100

    C:/Program Files/Git/hero endpoint working with graphQL

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// import { userInfo } from 'os';
+
 const { token } = require('./userToken.json');
 const express = require('express');
 const graphql = require('graphql-request');
@@ -13,10 +15,58 @@ const graphqlClient = new graphql.GraphQLClient('https://api.github.com/graphql'
   },
 });
 
-function createRepos(data) {
-  const repos = [];
-  data.user.repositories.nodes.array.forEach(element => repos.push(element));
-  return repos;
+function createHero(data) {
+  const achievments = {
+    commits: data.user.repository.ref.target.history.totalCount,
+    issues: data.user.repository.issues.totalCount,
+    repos: data.user.repositories.totalCount,
+    followers: data.user.followers.totalCount,
+    following: data.user.following.totalCount,
+    forks: data.user.repository.forkCount,
+  };
+  const userInfos = {
+    id: data.user.id,
+    username: data.user.login,
+    name: data.user.name,
+    url: data.user.url,
+    avatar: data.user.avatarUrl,
+    repo: data.user.repository.name,
+  };
+
+  const commitWeight = 1;
+  const issueWeight = 15;
+  const repoWeight = 50;
+  const followerWeight = 20;
+
+  const totalXP =
+        (achievments.commits * commitWeight) +
+        (achievments.issues * issueWeight) +
+        (achievments.repos * repoWeight) +
+        (achievments.followers * followerWeight);
+  const totalLevel = Math.round(0.3 - Math.sqrt(totalXP));
+  const totalHP = achievments.commits;
+  const totalAttack = Math.round(((1 / 3) * achievments.commits) + (achievments.issues * 5));
+  const totalDefense = achievments.repos * 15;
+  const totalSpeed = achievments.followers * 10;
+  const totalCharisma = (achievments.followers * 5) + (achievments.forks * 5);
+  const totalIntel = achievments.following;
+
+  const stats = {
+    xp: totalXP,
+    level: totalLevel,
+    hp: totalHP,
+    attack: totalAttack,
+    defense: totalDefense,
+    speed: totalSpeed,
+    charisma: totalCharisma,
+    intel: totalIntel,
+  };
+
+  return {
+    stats,
+    achievments,
+    userInfos,
+  };
 }
 
 app
@@ -56,6 +106,7 @@ app
     const query = `query($username:String!) {
       user(login: $username) {
         repositories(first: 30) {
+          totalCount
           pageInfo {
             hasNextPage
             endCursor
@@ -79,52 +130,52 @@ app
         res.send(error);
       });
   }).get('/hero', (req, res) => {
-    const context = {};
-    let issuesPromise;
-    fetchGithub(`https://api.github.com/users/${req.query.username}`)
-      .then((user) => {
-        context.userID = user.id;
-        context.username = user.login;
-        context.name = user.name;
-        context.avatar = user.avatar_url;
-        context.nbrFollowers = user.followers;
-        context.nbrFollowing = user.following;
-        return fetchGithub(`https://api.github.com/repos/${req.query.username}/${req.query.repo}`);
-      })
-      .then((repo) => {
-        context.repoName = repo.name;
-        context.nbrForks = repo.forks_count;
+    const variables = `{
+      "username": "${req.query.username}",
+      "repo": "${req.query.repo}"
+    }`;
 
-        // res.send(context);
-        // Saving the issues promise for future use
-        const number = '{/number}';
-        const issuesUrl = repo.issues_url.substring(
-          0,
-          repo.issues_url.length - number.length
-        );
-        issuesPromise = fetchGithub(issuesUrl);
+    const query = `query($username: String!, $repo: String!) {
+      user(login: $username) {
+        id
+        login
+        name
+        url
+        avatarUrl
+        repositories {
+          totalCount
+        }
+        followers {
+          totalCount
+        }
+        following {
+          totalCount
+        }
+        repository(name: $repo) {
+          name
+          forkCount
+          issues {
+            totalCount
+          }
+          ref(qualifiedName: "master") {
+            target {
+              ... on Commit {
+                history {
+                  totalCount
+                }
+              }
+            }
+          }
+        }
+      }
+    }`;
 
-        // Creating the url for accessing the list of commits
-        const sha = '{/sha}';
-        const commitsUrl = repo.commits_url.substring(
-          0,
-          repo.commits_url.length - sha.length
-        );
-
-        return fetchGithub(commitsUrl);
-      })
-      .then((commits) => {
-        context.nbrCommits = commits.length;
-        return issuesPromise;
-      })
-      .then((issues) => {
-        context.nbrIssues = issues.length;
-
-        res.send(context);
+    graphqlClient.request(query, variables)
+      .then((response) => {
+        const hero = createHero(response);
+        res.send(hero);
       })
       .catch((error) => {
-        console.log('error:');
-        console.log(error);
         res.send(error);
       });
   })
