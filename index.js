b21efbb75dc31e0b3c08a278b63ee5a6d89d991d
Lou <luana.martelli@heig-vd.ch>
2018-01-22 20:43:27 +0100

    Move to heroku

--- index.js
+++ index.js
@@ -40,4 +40,5 @@ app
   // .set('views', path.join(__dirname, 'views'))
   // .set('view engine', 'ejs')
   // .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+  .use('/', express.static(__dirname + '/public'));;
