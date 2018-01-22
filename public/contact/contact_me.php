9aa48651564bd9ee225971fb89ddbdfb2d8efbd4
Lou <luana.martelli@heig-vd.ch>
2018-01-22 22:00:49 +0100

    Move to heroku

--- public/contact/contact_me.php
+++ public/contact/contact_me.php
@@ -12,7 +12,7 @@ if(empty($_POST['name'])  		||
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-	
+
 // Create the email and send the message
 $to = 'tweb.is.fun@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
