ad73b3320c809c0e6f3af2c5c8da87a9740d3ad3
MiguelPomboDias <miguel.pombodias@heig-vd.ch>
2018-01-23 13:58:07 +0100

    better website

--- public/js/fetchHero.js
+++ public/js/fetchHero.js
@@ -1,29 +1,31 @@
 // Ici faire l'appel ajax jquery au gimme
+/* global $ */
 
 function fetchRepos() {
-  $.get( "https://quiet-depths-67700.herokuapp.com/hero", { username } )
-      .done(function( data ) {
-        alert( "Data Loaded: " + data );
-      })
+  const username = $('#inputPassword').val();
+  $.get('https://quiet-depths-67700.herokuapp.com/repos', { username })
+    .done((data) => {
+      alert(`Data Loaded: ${data}`);
+    });
 }
 
 function fetchHero(repo) {
-    let username = $('#inputPassword').val();
+  const username = $('#inputPassword').val();
 
-    $.get( "https://quiet-depths-67700.herokuapp.com/hero", { username } )
-      .done(function( data ) {
-        alert( "Data Loaded: " + data );
-      })
+  $.get('https://quiet-depths-67700.herokuapp.com/hero', { username })
+    .done((data) => {
+      alert(`Data Loaded: ${data}`);
+    });
 
-    let hero = {
-      'xp':10000,
-      'level':5,
-      'pv':100,
-      'attack':10,
-      'defense':10,
-      'speed':2,
-      'charisma':5,
-      'intel':3
-    };
+  const hero = {
+    xp: 10000,
+    level: 5,
+    pv: 100,
+    attack: 10,
+    defense: 10,
+    speed: 2,
+    charisma: 5,
+    intel: 3,
+  };
 }
 
--- public/js/graphUser.js
+++ public/js/graphUser.js
@@ -1,112 +1,111 @@
+/* global d3 */
 function graphUser(hero) {
+  const width = 300;
+  const height = 300;
+  const barHeight = (height / 2) - 40;
 
-  var width = 400,
-    height = 400,
-    barHeight = height / 2 - 40;
+  const formatNumber = d3.format('%');
 
-  var formatNumber = d3.format("%");
+  const color = d3.scale.ordinal()
+    .range(['#8dd3c7', '#bebada', '#fb8072', '#80b1d3', '#fdb462', '#b3de69', '#fccde5', '#d9d9d9', '#bc80bd', '#ccebc5', '#ffed6f']);
 
-  var color = d3.scale.ordinal()
-    .range(["#8dd3c7", "#ffffb3", "#bebada", "#fb8072", "#80b1d3", "#fdb462", "#b3de69", "#fccde5", "#d9d9d9", "#bc80bd", "#ccebc5", "#ffed6f"]);
+  const svg = d3.select('.d3Graph').append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g')
+    .attr('transform', `translate(${width / 2},${height / 2})`);
 
-  var svg = d3.select('.d3Graph').append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-  var extent = d3.extent(hero, function (d) { return d.value; });
-  var barScale = d3.scale.linear()
+  const extent = d3.extent(hero, d => d.value);
+  const barScale = d3.scale.linear()
     .domain(extent)
     .range([0, barHeight]);
 
-  var keys = hero.map(function (d, i) { return d.name; });
-  var numBars = keys.length;
+  const keys = hero.map((d, i) => d.name);
+  const numBars = keys.length;
 
-  var x = d3.scale.linear()
+  const x = d3.scale.linear()
     .domain(extent)
     .range([0, -barHeight]);
 
-  var xAxis = d3.svg.axis()
-    .scale(x).orient("left")
+  const xAxis = d3.svg.axis()
+    .scale(x).orient('left')
     .ticks(3)
     .tickFormat(formatNumber);
 
-  var circles = svg.selectAll("circle")
+  const circles = svg.selectAll('circle')
     .data(x.ticks(3))
-    .enter().append("circle")
-    .attr("r", function (d) { return barScale(d); })
-    .style("fill", "none")
-    .style("stroke", "black")
-    .style("stroke-dasharray", "2,2")
-    .style("stroke-width", ".5px");
+    .enter().append('circle')
+    .attr('r', d => barScale(d))
+    .style('fill', 'none')
+    .style('stroke', 'black')
+    .style('stroke-dasharray', '2,2')
+    .style('stroke-width', '.5px');
 
-  var arc = d3.svg.arc()
-    .startAngle(function (d, i) { return (i * 2 * Math.PI) / numBars; })
-    .endAngle(function (d, i) { return ((i + 1) * 2 * Math.PI) / numBars; })
+  const arc = d3.svg.arc()
+    .startAngle((d, i) => (i * 2 * Math.PI) / numBars)
+    .endAngle((d, i) => ((i + 1) * 2 * Math.PI) / numBars)
     .innerRadius(0);
 
-  var segments = svg.selectAll("path")
+  const segments = svg.selectAll('path')
     .data(hero)
-    .enter().append("path")
-    .each(function (d) { d.outerRadius = 0; })
-    .style("fill", function (d) { return color(d.name); })
-    .attr("d", arc);
+    .enter().append('path')
+    .each((d) => { d.outerRadius = 0; })
+    .style('fill', d => color(d.name))
+    .attr('d', arc);
 
-  segments.transition().ease("elastic").duration(1000).delay(function (d, i) { return (25 - i) * 100; })
-    .attrTween("d", function (d, index) {
-      var i = d3.interpolate(d.outerRadius, barScale(+d.value));
-      return function (t) { d.outerRadius = i(t); return arc(d, index); };
+  segments.transition().ease('elastic').duration(1000).delay((d, i) => (25 - i) * 100)
+    .attrTween('d', (d, index) => {
+      const i = d3.interpolate(d.outerRadius, barScale(+d.value));
+      return (t) => { d.outerRadius = i(t); return arc(d, index); };
     });
 
-  svg.append("circle")
-    .attr("r", barHeight)
-    .classed("outer", true)
-    .style("fill", "none")
-    .style("stroke", "black")
-    .style("stroke-width", "1.5px");
+  svg.append('circle')
+    .attr('r', barHeight)
+    .classed('outer', true)
+    .style('fill', 'none')
+    .style('stroke', 'black')
+    .style('stroke-width', '1.5px');
 
-  var lines = svg.selectAll("line")
+  const lines = svg.selectAll('line')
     .data(keys)
-    .enter().append("line")
-    .attr("y2", -barHeight - 20)
-    .style("stroke", "black")
-    .style("stroke-width", ".5px")
-    .attr("transform", function (d, i) { return "rotate(" + (i * 360 / numBars) + ")"; });
+    .enter().append('line')
+    .attr('y2', -barHeight - 20)
+    .style('stroke', 'black')
+    .style('stroke-width', '.5px')
+    .attr('transform', (d, i) => `rotate(${(i * 360) / numBars})`);
 
-  svg.append("g")
-    .attr("class", "x axis")
+  svg.append('g')
+    .attr('class', 'x axis')
     .call(xAxis);
 
   // Labels
-  var labelRadius = barHeight * 1.025;
+  const labelRadius = barHeight * 1.025;
 
-  var labels = svg.append("g")
-    .classed("labels", true);
+  const labels = svg.append('g')
+    .classed('labels', true);
 
-  labels.append("def")
-    .append("path")
-    .attr("id", "label-path")
-    .attr("d", "m0 " + -labelRadius + " a" + labelRadius + " " + labelRadius + " 0 1,1 -0.01 0");
+  labels.append('def')
+    .append('path')
+    .attr('id', 'label-path')
+    .attr('d', `m0 ${-labelRadius} a${labelRadius} ${labelRadius} 0 1,1 -0.01 0`);
 
-  labels.selectAll("text")
+  labels.selectAll('text')
     .data(keys)
-    .enter().append("text")
-    .style("text-anchor", "middle")
-    .style("font-weight", "bold")
-    .style("fill", function (d, i) { return "#3e3e3e"; })
-    .append("textPath")
-    .attr("xlink:href", "#label-path")
-    .attr("startOffset", function (d, i) { return i * 100 / numBars + 50 / numBars + '%'; })
-    .text(function (d) { return d.toUpperCase(); });
-
+    .enter().append('text')
+    .style('text-anchor', 'middle')
+    .style('font-weight', 'bold')
+    .style('fill', (d, i) => '#3e3e3e')
+    .append('textPath')
+    .attr('xlink:href', '#label-path')
+    .attr('startOffset', (d, i) => `${((i * 100) / numBars) + (50 / numBars)}%`)
+    .text(d => d.toUpperCase());
 }
 
 graphUser([
-  { name: "PV", value: .10 },
-  { name: "ATTAQUE", value: .15 },
-  { name: "DEFENSE", value: .05 },
-  { name: "VITESSE", value: .08 },
-  { name: "CHARISME", value: .03 },
-  { name: "INTELLIGENCE", value: .12 },
+  { name: 'PV', value: 0.10 },
+  { name: 'ATTAQUE', value: 0.15 },
+  { name: 'DEFENSE', value: 0.05 },
+  { name: 'VITESSE', value: 0.08 },
+  { name: 'CHARISME', value: 0.03 },
+  { name: 'INTELLIGENCE', value: 0.12 },
 ]);
